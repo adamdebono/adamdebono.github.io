85ade7284a4f7783c3182ff01a40d4eb8fbd1e8d
Adam Debono <adam@adebono.com>
2014-06-18 14:05:21 +1000

    social + content

--- directives/directives.js
+++ directives/directives.js
@@ -4,7 +4,7 @@ angular.module('adamdebono')
 			restrict: 'E',
 			replace: true,
 			templateUrl: 'directives/navigation.html',
-			controller: function($scope, $element) {
+			controller: function($scope) {
 				$scope.navItems = [
 					{
 						title: 'Home',
@@ -23,5 +23,45 @@ angular.module('adamdebono')
 					$scope.path = $location.path();
 				});
 			}
-		}
+		};
+	}])
+	.directive('social', [function() {
+		return {
+			restrict: 'E',
+			replace: true,
+			templateUrl: 'directives/social.html',
+			controller: function($scope) {
+				$scope.items = [
+					{
+						title: 'Twitter',
+						account: '@adamdebono',
+						icon: 'fa-twitter-square',
+						href: 'http://twitter.com/adamdebono'
+					},{
+						title: 'GitHub',
+						account: 'adamdebono',
+						icon: 'fa-github-square',
+						href: 'http://github.com/adamdebono'
+					},{
+						title: 'LinkedIn',
+						account: 'adamdebono',
+						icon: 'fa-linkedin-square',
+						href: 'http://www.linkedin.com/in/adamdebono'
+					},{
+						title: 'eMail',
+						account: 'adam@adebono.com',
+						icon: 'fa-envelope-square',
+						href: 'mailto:adam@adebono.com'
+					}
+				]
+			}
+		};
+	}])
+	.directive('contentBox', [function() {
+		return {
+			restrict: 'E',
+			replace: true,
+			templateUrl: 'directives/content-box.html',
+			transclude: true
+		};
 	}]);
